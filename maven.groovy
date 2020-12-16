209189c80f731be7de4336a5340f427079a06ec4
rodrigozunigab <rodrigozunigab@gmail.com>
2020-12-15 21:58:08 -0300

    se agrega opcion de seleccionar el metodo de compilacion

--- maven.groovy
+++ maven.groovy
@@ -7,31 +7,21 @@
 def call(){
   
       stage('Compile Code') {
-            steps {
                 sh 'mvn clean compile -e'
-            }
         }
-        stage('Test Code') {
-            steps {              
+        stage('Test Code') {           
                 sh 'mvn clean test -e'
-            }
         }
-        stage('Jar ') {
-            steps {               
-                sh 'mvn clean package -e'
-            }
+        stage('Jar ') {             
+                sh 'mvn clean package -e' 
         }
         stage('SonarQube analysis') {
-           steps {
-             withSonarQubeEnv(installationName: 'sonar') { 
+             withSonarQubeEnv(installationName: 'sonar-server') { 
              sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-             }
           }
         }
-        stage('Upload Nexus Dev2') {
-            steps {               
+        stage('Upload Nexus') {            
                 nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/servidorcasa/Documents/Cursos/2020_devops/ejemplo_maven_24_11_2020/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '3.0.1']]]
-            }
         } 
 
 }
